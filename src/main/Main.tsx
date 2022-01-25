57f84ced0845a661f5c0ddb7abcf414c50ca319a
Pavel <sosnovskiy.pavel@inbox.ru>
2022-01-25 20:54:14 +0300

    Update Main.tsx

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Aside } from "../aside/Aside";
-
-import style from '../aside/Aside.module.scss';
-
-
+import s from './../aside/Aside.module.scss'
+import { AsideAboutMe } from "../aside/asideAboutMe/AsideAboutMe";
+import { AsideSkills } from "../aside/asideSkills/AsideSkills";
+import { AsideProject } from "../aside/asideProject/AsideProject";
+export type AsideStyleType = {
+    className: string
+}
 export const Main = () => {
     return (
         <main>
-            <Aside className={style.aside} />
-            <Aside className={style.aside}/>
-            <Aside className={style.aside}/>
-            <Aside className={style.aside}/>
-        </main>
-        
+            <AsideAboutMe className= {`${s.aside} ${s.aside__block_red}`}/>
+            <AsideSkills className= {`${s.aside} ${s.aside__block_green}`}/>
+            <AsideProject className= {`${s.aside} ${s.aside__block_blue}`}/>
+
+        </main>        
     )
 }
