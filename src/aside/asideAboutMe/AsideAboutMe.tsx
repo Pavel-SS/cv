c65bbfde45e839aa049a6cc477caff6e114affc5
Pavel <sosnovskiy.pavel@inbox.ru>
2022-01-25 20:53:47 +0300

    Create AsideAboutMe.tsx

---
+++ src/aside/asideAboutMe/AsideAboutMe.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AsideStyleType } from "../../main/Main";
+import style from './AsideAboutMe.module.scss'
+
+export const AsideAboutMe = (props: AsideStyleType) => {
+    return(
+        <div className={props.className}>
+            <div className={`section ${style.about_me__container}`}>
+                <div className={style.about_me__text}>
+                    <h1 className={style.about_me__title}>My name Pavel Sasnouski</h1>
+                    <h2 className={style.about_me__descr}>I front-end developer</h2>
+                </div>
+                <div className={style.about_me__foto}>
+                </div>
+            </div>
+        </div>
+    )
+}
