406c3c79a77283cd4b1f301296b7966d0fa1577a
Pavel <sosnovskiy.pavel@inbox.ru>
2022-01-25 23:25:11 +0300

    Update AsideProject.tsx

--- src/aside/asideProject/AsideProject.tsx
+++ src/aside/asideProject/AsideProject.tsx
@@ -2,15 +2,18 @@ import React from "react";
 
 import { AsideStyleType } from "../../main/Main";
 import s from '../../aside/Aside.module.scss'
+import { Project } from "./project/Project";
 
 export const AsideProject = (props: AsideStyleType) => {
     return (
         <aside className={props.className}>
             <div className={`section ${s.aside__container}`}>
                 <div className={s.aside__title}>
-                    <h2>My skills</h2>
+                    <h2>My project</h2>
                 </div>
                 <div className={s.aside__item}>
+                    <Project/>
+                    <Project/>
                 </div>
             </div>
         </aside>
