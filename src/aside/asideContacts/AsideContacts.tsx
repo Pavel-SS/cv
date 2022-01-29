357ec0758b31e584b2a6b18b4bcf66557bb2f353
Pavel <sosnovskiy.pavel@inbox.ru>
2022-01-29 23:58:47 +0300

    Update AsideContacts.tsx

--- src/aside/asideContacts/AsideContacts.tsx
+++ src/aside/asideContacts/AsideContacts.tsx
@@ -1,1 +1,26 @@
 import React from "react";
+
+import { AsideStyleType } from "../../main/Main";
+import s from '../../aside/Aside.module.scss';
+import style from './AsideContacts.module.scss';
+
+export const AsideContacts = (props: AsideStyleType) => {
+    return (
+        <aside className={props.className}>
+            <form className={`section ${s.aside__container}`}>
+                <fieldset className={`${style.aside_contacts}`}>
+                    <legend>Contacts</legend>
+                    <label>
+                        E-mail:
+                        <input type="email" name="e-mail" placeholder="company@domain.by" required />
+                    </label>
+                    <label>
+                        Phoe:
+                        <input type="tel" name="tel" placeholder="+375(25)XXX-XX-XX" required />
+                    </label>
+                </fieldset>
+                <button className={s.aside__btn} name="submit">Send</button>
+            </form>
+        </aside>
+    )
+}
