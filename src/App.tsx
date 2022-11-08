0437902a853aaa0591bb5ebee35d2b05ab992cd2
Pavel Sasnouski <sosnovskiy.paul@gmail.com>
2022-11-08 23:30:57 +0300

    fix style

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import 'normalize.css';
 import { Header } from './header/Header';
 import { Main } from './main/Main';
 import { Footer } from './footer/Footer';
+
 //Style
 import '../src/style/GeneralStyle.scss'
 
