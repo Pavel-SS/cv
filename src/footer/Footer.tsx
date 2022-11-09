97858140bd5cb4bc851fb9f6aa269ee213b6d9e8
Pavel Sasnouski <sosnovskiy.paul@gmail.com>
2022-11-09 15:11:19 +0300

    add link CV

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -6,7 +6,7 @@ import style from './Footer.module.scss'
 import GitHubLogo from './../assets/icon/github.svg';
 import LinkedInLogo from './../assets/icon/linkedin.svg';
 import TelegramLogo from './../assets/icon/telegram.svg';
-
+import CV from './../assets/icon/clipboard.svg'
 export const Footer = () => {
     return (
         <footer className={s.footer}>
@@ -23,13 +23,18 @@ export const Footer = () => {
                         </a>
                     </li>
                     <li>
-                        <a href="#" className={style.footer__link_contact}>
+                        <a href="https://www.linkedin.com/in/pavel-sosnovskiy-0013" className={style.footer__link_contact}>
                             <img src={LinkedInLogo} alt="LinkedIn" />
                         </a>
                     </li>
                     <li>
-                        <a href="#" className={style.footer__link_contact}>
+                        <a href="https://t.me/Pavel_13S" className={style.footer__link_contact}>
                             <img src={TelegramLogo} alt="Telegram" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://drive.google.com/file/d/1K-SjbtfcjlShM-MiaHKXA7hp2hqSDlpG/view?usp=drivesdk" className={style.footer__link_contact}>
+                            <img src={CV} alt="CV" />
                         </a>
                     </li>
                 </ul>
